9c420ec0d4f48cbab31d5cbd5ebd23121858590c
norrova <norrova@gmail.com>
2020-05-21 10:24:32 +0200

    add skeleton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lyppy",
+    version="0.1",
+    author="Norro valentin",
+    author_email="norrova.pro@gmail.com",
+    packages=find_packages()
+)
